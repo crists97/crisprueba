e890cedca4e8962130e304a9eb5533b33c96f5f6
Cristina Trilla <mcriseveris@gmail.com>
2021-11-15 12:18:47 +0000

    NUEVO DESPLIEGUE

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,26 +1,35 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "com/crisp/crisprueban/services/ConfigHelper",
+    "com/crisp/crisprueban/services/AjaxCaller",
+    "sap/ui/model/json/JSONModel",
+    "sap/base/util/UriParameters",
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-    function (Controller,ConfigHelper) {
+    function (Controller, ConfigHelper, AjaxCaller, JSONModel, UriParameters) {
         "use strict";
 
         return Controller.extend("com.crisp.crisprueban.controller.View1", {
             onInit: function () {
-              //  this.getInfoService();
+                var urlWindows = window["location"]["href"];
+                urlWindows = urlWindows.split("/index")[0]
+                var url = urlWindows + this.getView().getParent().getModel("undefined").sMetadataUrl;
+                var that = this;
+                setTimeout(function (that) {
+                    AjaxCaller.getInstance()
+                        .requestAjax("GET", url)
+                        .then(function (oData) {
+                            return oData;
+                        });
+                }, 2000);
+
             },
 
-            getInfoService: function (sProdctId) {
-                var oUserDataInfo = ConfigHelper.getInstance().getProductDataInfo();
+            getInfoService: function (sMockServerUrl) {
 
-                return AjaxCaller.getInstance()
-                    .requestAjax(oUserDataInfo.method, oUserDataInfo.url)
-                    .then(function (oData) {
-                        return oData;
-                    });
+
             }
         });
     });
