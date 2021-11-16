38efee6a493644108eec1d8ef014e8e9c16ed89f
Cristina Trilla <mcriseveris@gmail.com>
2021-11-16 14:34:45 +0000

    subida

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,25 +1,33 @@
 sap.ui.define([
-        "sap/ui/core/mvc/Controller",
-        "com/crisp/crisprueban/services/ConfigHelper",
-        "com/crisp/crisprueban/services/AjaxCaller",
-        "com/crisp/crisprueban/services/ApiFacade",
-        "sap/ui/model/json/JSONModel",
-        "sap/base/util/UriParameters",
-        "com/crisp/crisprueban/base/BaseObject"
-    ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function(Controller, ConfigHelper, AjaxCaller, ApiFacade, JSONModel, UriParameters, BaseObject) {
+    "sap/ui/core/mvc/Controller",
+    "com/crisp/crisprueban/services/ConfigHelper",
+    "com/crisp/crisprueban/services/AjaxCaller",
+    "com/crisp/crisprueban/services/ApiFacade",
+    "sap/ui/model/json/JSONModel",
+    "sap/base/util/UriParameters",
+    "com/crisp/crisprueban/base/BaseObject", 'sap/m/MessageToast'
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+    function (Controller, ConfigHelper, AjaxCaller, ApiFacade, JSONModel, UriParameters, BaseObject, MessageToast) {
         "use strict";
 
         return Controller.extend("com.crisp.crisprueban.controller.View1", {
+            ApiFacade: ApiFacade,
+            onInit: function () {
+            },
 
-            onInit: function() {},
-
-            getCustomersData: function(sMockServerUrl) {
-                ApiFacade.getInstance().getCustomersData()
-
+            getCustomersData: function (sMockServerUrl) {
+                return ApiFacade.getInstance()
+                    .getCustomersData()
+                    .then(function (oData) {
+                        MessageToast.show("THERE ARE ->  " +
+                            oData.d.results.length + " <- CUSTOMER");
+                        return oData;
+                    });
             }
+
+
         });
     });
