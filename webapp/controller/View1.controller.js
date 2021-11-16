156f264c81c69736bec188b29e4b9b30c54a333e
Cristina <mcriseveris@gmail.com>
2021-11-16 13:55:14 +0100

    prueba

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,24 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "com/crisp/crisprueban/services/ConfigHelper",
-    "com/crisp/crisprueban/services/AjaxCaller",
-    "com/crisp/crisprueban/services/ApiFacade",
-    "sap/ui/model/json/JSONModel",
-    "sap/base/util/UriParameters",
-    "com/crisp/crisprueban/base/BaseObject"
-],
-	/**
-	 * @param {typeof sap.ui.core.mvc.Controller} Controller
-	 */
-    function (Controller, ConfigHelper, AjaxCaller, ApiFacade, JSONModel, UriParameters, BaseObject) {
+        "sap/ui/core/mvc/Controller",
+        "com/crisp/crisprueban/services/ConfigHelper",
+        "com/crisp/crisprueban/services/AjaxCaller",
+        "com/crisp/crisprueban/services/ApiFacade",
+        "sap/ui/model/json/JSONModel",
+        "sap/base/util/UriParameters",
+        "com/crisp/crisprueban/base/BaseObject"
+    ],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function(Controller, ConfigHelper, AjaxCaller, ApiFacade, JSONModel, UriParameters, BaseObject) {
         "use strict";
 
         return Controller.extend("com.crisp.crisprueban.controller.View1", {
-            ApiFacade: ApiFacade,
-            onInit: function () { 
-            },
 
-            getCustomersData: function (sMockServerUrl) {
+            onInit: function() {},
+
+            getCustomersData: function(sMockServerUrl) {
                 ApiFacade.getInstance().getCustomersData()
 
             }
